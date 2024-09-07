63924896f4248ae2f4bbc0dfe735475c5b778bd5
usermxnny <mannyvillanueva1021@gmail.com>
2024-09-06 19:42:09 -0500

    Add files via upload

---
+++ script2.py
@@ -0,0 +1,11 @@
+def main():
+    print("Lab 1: Getting Started with Python")
+    print("CIS 103: Introduction to Programming")
+    print("Instructor: Manuel Villanueva")
+    print("Student Name: Manuel Villanueva")
+    print("Date: 08/23/24")
+    print("Description:")
+    print("This script prints a greeting message to the user")
+
+if __name__ == "__main__":
+    main()
